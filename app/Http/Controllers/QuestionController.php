d88a8acd5579e3733ca4f01d0a4dc0225a0d70e2
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-14 15:28:01 -0700

    chore: escrita dos metodos de CRUD

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,13 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //metodo responsavel por testar a requisao da rota API
-    public function getData(){
+    //metodo responsavel por recupera a lista das perguntas
+    public function index (Request $request){
 
-        return response()->json(['Requisicao realizada com sucesso...']);
+        return response()->json(['sucess' => 'Perguntas retomadas com sucesso'], status:200);
     }
+
+    //metodo responsavel por adicionar novas perguntas
+    public function store(Request $request){
+
+        return response()->json(['error' => 'Do momento, nao e possivel adicionar perguntas'], status:403);
+    }
+
+    //metodo para mostar detalhes de uma pergunta
+    public function show (Request $request, Question $question){
+
+        return response()->json(['error' => 'Do momento, nao e possivel mostrar detalhes de uma pergunta'], status:403);
+    }
+
+    //metodo para fazer update de uma pergunta
+    public function update (Request $request, Question $question){
+
+        return response()->json(['error' => 'Do momento, nao e possivel actualizar a pergunta'], status:403);
+    }
+
+    //metodo para remover uma pergunta
+    public function destroy (Request $request, Question $question){
+
+        return response()->json(['error' => 'Do momento, nao e possivel remover a pergunta'], status:403);
+    }
+
 }
--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -1,14 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    //metodo responsavel por testar a requisao da rota API
-    public function getData(){
+    //metodo responsavel por recupera a lista das perguntas
+    public function index (Request $request){
 
-        return response()->json(['Requisicao realizada com sucesso...']);
+        return response()->json(['sucess' => 'Disciplinas retomadas com sucesso'], status:200);
+    }
+
+    //metodo responsavel por adicionar novas perguntas
+    public function store(Request $request){
+
+        return response()->json(['error' => 'Do momento, nao e possivel adicionar Disciplinas'], status:403);
+    }
+
+    //metodo para mostar detalhes de uma pergunta
+    public function show (Request $request, Subject $subject){
+
+        return response()->json(['error' => 'Do momento, nao e possivel mostrar detalhes de uma Disciplina'], status:403);
+    }
+
+    //metodo para fazer update de uma pergunta
+    public function update (Request $request, Subject $subject){
+
+        return response()->json(['error' => 'Do momento, nao e possivel actualizar a Disciplina'], status:403);
+    }
+
+    //metodo para remover uma pergunta
+    public function destroy (Request $request, Subject $subject){
+
+        return response()->json(['error' => 'Do momento, nao e possivel remover a Disciplina'], status:403);
     }
 }
